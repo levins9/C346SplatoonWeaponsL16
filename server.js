6860e21b7d1fe4d857e7deeb85b72808833d3c53
levins9 <24017226@myrp.edu.sg>
2026-01-08 14:27:02 +0800

    new

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require ('express');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+const port = 3000;
+
+//database config info
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 100,
+    queueLimit: 0,
+};
+
+const app = express();
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log('Server running on port', port)
+});
+
+app.get('/allweapons', async (req, res) => {
+    try{
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('SELECT * FROM defaultdb.weapons');
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error for allweapons'});
+    }
+})
+app.post('/addweapon', async (req, res) => {
+    const {weapon_name, weapon_pic} =req.body;
+    try {
+        let connection = await mysql.createConnection({dbConfig});
+        await connection.execute('INSERT INTO cards (weapon_name, weapon_pic) VALUES (?,?)',[weapon_name, weapon_pic]);
+        res.status (201).json({message: 'Card' + card_name + 'successfully added'});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error - could not add '+ weapon_name });
+    }
+})
